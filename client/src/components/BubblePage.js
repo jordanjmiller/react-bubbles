c6e8e233d50f551fe4789e4bbc2142371545aafd
Jordan Miller <jordan.j.miller@live.com>
2019-11-15 09:31:33 -0800

    add color stretch complete

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -26,3 +26,4 @@ const BubblePage = () => {
 };
 
 export default BubblePage;
+
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -10,6 +10,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -45,6 +46,14 @@ const ColorList = ({ colors, updateColors }) => {
     })
     .catch(err => { console.log(err.response.data.error) });
   };
+  const addItem = e => {
+    e.preventDefault();
+    axiosWithAuth().post(`/api/colors/`, colorToAdd)
+    .then(res => { console.log(res);
+      updateColors([...res.data]);
+    })
+    .catch(err => { console.log(err.response.data.error) });
+  };
 
   return (
     <div className="colors-wrap">
@@ -52,20 +61,8 @@ const ColorList = ({ colors, updateColors }) => {
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
-            <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
-              </span>{" "}
-              {color.color}
-            </span>
-            <div
-              className="color-box"
-              style={{ backgroundColor: color.code.hex }}
-            />
+            <span> <span className="delete" onClick={e => { e.stopPropagation(); deleteColor(color) } }> x </span>{" "} {color.color} </span>
+            <div className="color-box" style={{ backgroundColor: color.code.hex }} />
           </li>
         ))}
       </ul>
@@ -74,24 +71,12 @@ const ColorList = ({ colors, updateColors }) => {
           <legend>edit color</legend>
           <label>
             color name:
-            <input
-              onChange={e =>
-                setColorToEdit({ ...colorToEdit, color: e.target.value })
-              }
-              value={colorToEdit.color}
-            />
+            <input onChange={e => setColorToEdit({ ...colorToEdit, color: e.target.value }) } value={colorToEdit.color} />
           </label>
           <label>
             hex code:
             <input
-              onChange={e =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: { hex: e.target.value }
-                })
-              }
-              value={colorToEdit.code.hex}
-            />
+              onChange={e => setColorToEdit({ ...colorToEdit, code: { hex: e.target.value }  }) } value={colorToEdit.code.hex} />
           </label>
           <div className="button-row">
             <button type="submit">save</button>
@@ -99,8 +84,24 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
+      {/* <div className="spacer" /> */}
       {/* stretch - build another form here to add a color */}
+      <form onSubmit={addItem}>
+          <legend>add color</legend>
+          <label>
+            color name:
+            <input onChange={e => setColorToAdd({ ...colorToAdd, color: e.target.value }) } value={colorToAdd.color} />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e => setColorToAdd({ ...colorToAdd, code: { hex: e.target.value }  }) } value={colorToAdd.code.hex} />
+          </label>
+          <div className="button-row">
+            <button type="submit">save</button>
+            <button onClick={() => setEditing(false)}>cancel</button>
+          </div>
+        </form>
     </div>
   );
 };
